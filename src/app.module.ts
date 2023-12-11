d22f06f4bb5b45592c8719c3c017d66d1f969db1
faizttn <faijuddin@tothenew.com>
2023-12-11 16:04:08 +0530

    postgresql database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AuthModule } from './user/auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -11,6 +12,16 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.prod.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'nestjs-db',
+      // entities: [],
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
