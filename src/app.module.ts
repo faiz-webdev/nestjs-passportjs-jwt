5a3c49850654bd040fa3fa35271e304b7579d5dd
faizttn <faijuddin@tothenew.com>
2023-12-11 15:13:36 +0530

    configure environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './user/auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.local.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
